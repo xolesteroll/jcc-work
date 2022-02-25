6b3d4493fe853f35d4c5dff8c0fc0556d4dba111
Anton <Iarovoi>
2022-02-25 14:40:47 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $(function () {
+        var includes = $('[data-include]')
+        $.each(includes, function () {
+          var file = $(this).data('include') + '.html'
+          $(this).load(file)
+        })
+      })
+      
+});
+
